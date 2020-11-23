fb3092395cf1d8a4f2c809f1705b565d43be891f
RamziBach <ramzibach.dev@gmail.com>
2020-11-23 00:47:50 -0500

    code review

--- src/components/TodoApp.js
+++ src/components/TodoApp.js
@@ -9,130 +9,164 @@ import { themeContext } from '../context/ThemeContextProvider';
 
 const TodoApp = () => {
   const { light } = useContext(themeContext);
-  const initialState = () => JSON.parse(localStorage.getItem('Tasks')) || [];
-  const [ tasks, setTasks ] = useState(initialState);
-  const [ newTask, setNewTask ] = useState('');
-  const [ isEditing, setIsEditing ] = useState(false);
-  const [ editId, setEditId ] = useState('');
+  const LOCAL_STORAGE = () => JSON.parse(localStorage.getItem('Tasks')) || [];
+  const [tasks, setTasks] = useState(LOCAL_STORAGE);
+  const [text, setText] = useState('');
+  const [isEditing, setIsEditing] = useState(false);
+  const [editId, setEditId] = useState('');
 
   const inputRef = useRef(null);
 
-  const handleChange = e => {
-    const { value } = e.target;
-    setNewTask(prevState => prevState = value);
-  };
+  const handleChange = e => setText(e.target.value);
 
   const handleSubmit = e => {
     e.preventDefault();
-    if (newTask === '') return;
-    if (!isEditing) {
-      const newTaskArr = [...tasks, {id: uuidv4(), title: newTask, completed: false}];
-      setTasks(prevState => prevState = newTaskArr);
-      setNewTask('');
-      inputRef.current.focus();
-    } else {
-      const newArr = tasks.slice();
-      const indexArr = newArr.map(arr => arr.id);
-      const index = indexArr.indexOf(editId);
-      newArr.splice(index, 1, {id: editId, title: newTask, completed: false});
-      setTasks(prevState => prevState = newArr);
-      setNewTask('');
+    inputRef.current.focus();
+    if (text.length === 0) return;
+    if (isEditing) {
+      setTasks(prevState => {
+        const newTasks = [...prevState];
+        const index = newTasks.findIndex(task => task.id === editId);
+        newTasks.splice(index, 1, { id: uuidv4(), text, completed: false });
+        return newTasks;
+      });
+      setText('');
       setEditId('');
       setIsEditing(false);
-      inputRef.current.focus();
+      return;
     }
+    setTasks(prevState => [
+      ...prevState,
+      { id: uuidv4(), text, completed: false },
+    ]);
+    setText('');
   };
 
   const handleClear = () => {
-    setTasks([]);
     inputRef.current.focus();
+    setTasks([]);
   };
 
   const handleCancel = () => {
+    inputRef.current.focus();
     setIsEditing(false);
-    setNewTask('');
+    setText('');
     setEditId('');
-    inputRef.current.focus();
   };
 
-  const handleDelete = id => {
+  const handleDelete = id =>
     setTasks(prevState => prevState.filter(task => task.id !== id));
-  };
 
-  const handleEdit = id => {
-    const item = tasks.find(task => task.id === id);
-    setNewTask(item.title);
+  const handleEdit = (id, text) => {
+    inputRef.current.focus();
+    setText(text);
     setIsEditing(true);
-    setEditId(item.id);
-    inputRef.current.focus();
+    setEditId(id);
   };
 
-  const handleCheck = (title, id) => {
-    if (tasks.find(task => task.id === id).completed) {
-      const newArr = tasks.slice();
-      const indexArr = newArr.map(arr => arr.id);
-      const index = indexArr.indexOf(id);
-      newArr.splice(index, 1, {id, title, completed: false});
-      setTasks(prevState => prevState = newArr);
-    } else {
-      const newArr = tasks.slice();
-      const indexArr = newArr.map(arr => arr.id);
-      const index = indexArr.indexOf(id);
-      newArr.splice(index, 1, {id, title, completed: true});
-      setTasks(prevState => prevState = newArr);
+  const handleCheck = (id, text, completed) => {
+    if (completed) {
+      setTasks(prevState => {
+        const newTasks = [...prevState];
+        const index = newTasks.findIndex(task => task.id === id);
+        newTasks.splice(index, 1, { id: uuidv4(), text, completed: false });
+        return newTasks;
+      });
+      return;
     }
+    setTasks(prevState => {
+      const newTasks = [...prevState];
+      const index = newTasks.findIndex(task => task.id === id);
+      newTasks.splice(index, 1, { id: uuidv4(), text, completed: true });
+      return newTasks;
+    });
   };
 
-  useEffect(() => {
-    localStorage.setItem('Tasks', JSON.stringify(tasks));
-  }, [tasks]);
+  useEffect(() => inputRef.current.focus(), []);
 
-  useEffect(() => {
-    inputRef.current.focus();
-  }, []);
+  useEffect(() => localStorage.setItem('Tasks', JSON.stringify(tasks)), [
+    tasks,
+  ]);
 
-  const liStyle = {textDecoration: 'line-through', fontWeight: '100', fontStyle: 'italic'};
-  const TaskLists = tasks.map(task => {
+  const liStyle = {
+    textDecoration: 'line-through',
+    fontWeight: '100',
+    fontStyle: 'italic',
+  };
+
+  const taskLists = tasks.map(task => {
     return (
-      <li className="list" style={task.completed ? liStyle : {textDecoration: 'none'}} key={task.id}>
-        {task.title}
+      <li
+        className="list"
+        style={task.completed ? liStyle : { textDecoration: 'none' }}
+        key={task.id}
+      >
+        {task.text}
         <div>
           <button
-            style={light ? {backgroundColor: 'white'} : {backgroundColor: '#212121'}}
-            title="Delete" 
-            className="btn" 
-            onClick={() => handleDelete(task.id)}><i className="fas fa-trash-alt"></i>
+            style={
+              light
+                ? { backgroundColor: 'white' }
+                : { backgroundColor: '#212121' }
+            }
+            title="Delete"
+            className="btn"
+            onClick={() => handleDelete(task.id)}
+            disabled={isEditing ? true : false}
+          >
+            <i className="fas fa-trash-alt"></i>
           </button>
           <button
-            style={light ? {backgroundColor: 'white'} : {backgroundColor: '#212121'}}
-            title="Edit" 
-            className="btn" 
-            onClick={() => handleEdit(task.id)}><i className="fas fa-pen-fancy"></i>
+            style={
+              light
+                ? { backgroundColor: 'white' }
+                : { backgroundColor: '#212121' }
+            }
+            title="Edit"
+            className="btn"
+            onClick={() => handleEdit(task.id, task.text)}
+            disabled={isEditing ? true : false}
+          >
+            <i className="fas fa-pen-fancy"></i>
           </button>
           <button
-            style={light ? {backgroundColor: 'white'} : {backgroundColor: '#212121'}}
-            title="Complete" 
-            className="btn" 
-            onClick={() => handleCheck(task.title, task.id)}><i className="fas fa-check"></i>
+            style={
+              light
+                ? { backgroundColor: 'white' }
+                : { backgroundColor: '#212121' }
+            }
+            title="Complete"
+            className="btn"
+            onClick={() => handleCheck(task.id, task.text, task.completed)}
+            disabled={isEditing ? true : false}
+          >
+            <i className="fas fa-check"></i>
           </button>
         </div>
       </li>
-    )
-  })
+    );
+  });
   return (
     <div className="TodoApp">
       <div className="todoapp_child">
         <Header />
         <TodoForm
           onSubmit={handleSubmit}
-          value={newTask}
+          value={text}
           onChange={handleChange}
           onClick={!isEditing ? handleClear : handleCancel}
           isEditing={isEditing}
           reference={inputRef}
         />
         <TodoList>
-          {tasks.length > 0 ? TaskLists : <span className="no-task"><i className="fas fa-tasks"></i><span className="no-task-p">Add tasks above</span></span>}
+          {tasks.length > 0 ? (
+            taskLists
+          ) : (
+            <span className="no-task">
+              <i className="fas fa-tasks"></i>
+              <span className="no-task-p">Add tasks above</span>
+            </span>
+          )}
         </TodoList>
       </div>
     </div>
--- src/context/ThemeContextProvider.js
+++ src/context/ThemeContextProvider.js
@@ -3,11 +3,11 @@ import React, { useState, useEffect } from 'react';
 const themeContext = React.createContext();
 
 const ThemeContextProvider = props => {
-  const [ light, setLight ] = useState(false);
+  const [light, setLight] = useState(false);
 
   const toggleLight = () => {
     setLight(prevState => !prevState);
-  }
+  };
 
   useEffect(() => {
     const body = document.querySelector('body');
@@ -21,7 +21,7 @@ const ThemeContextProvider = props => {
   }, [light]);
 
   return (
-    <themeContext.Provider value={{light, toggleLight}}>
+    <themeContext.Provider value={{ light, toggleLight }}>
       {props.children}
     </themeContext.Provider>
   );
